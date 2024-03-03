6ca8c62ddcf5a804d25434daa860cbbc5e3f1ac1
pionere <pionere@freemail.hu>
2024-03-03 11:20:46 +0100

    fix error message when the vulkan library is not found (uikit)

--- src/video/uikit/SDL_uikitvulkan.m
+++ src/video/uikit/SDL_uikitvulkan.m
@@ -102,14 +102,19 @@ int UIKit_Vulkan_LoadLibrary(SDL_VulkanVideo *vulkan_config, const char *path)
             (PFN_vkGetInstanceProcAddr)SDL_LoadFunction(
                                     vulkan_config->loader_handle,
                                     "vkGetInstanceProcAddr");
-    }
-
-    if (!vkGetInstanceProcAddr) {
-        SDL_SetError("Failed to find %s in either executable or %s: %s",
-                     "vkGetInstanceProcAddr",
-                     "linked Vulkan Portability library",
-                     (const char *) dlerror());
-        goto fail;
+        if (!vkGetInstanceProcAddr) {
+            const char *msg;
+            if (paths == defaultPaths) {
+                msg = "Failed to find %s in either executable or %s: %s";
+            } else {
+                msg = "Failed to find %s in %s: %s";
+            }
+            SDL_SetError(msg,
+                         "vkGetInstanceProcAddr",
+                         foundPath,
+                         (const char *) dlerror());
+            goto fail;
+        }
     }
 
     vulkan_config->vkGetInstanceProcAddr = vkGetInstanceProcAddr;
