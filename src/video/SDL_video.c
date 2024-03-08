d206df786dd884d1afd2ef691469558effce6927
pionere <pionere@freemail.hu>
2024-03-08 08:15:52 +0100

    merge SDL_clipboard.c into SDL_video.c

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -4559,7 +4559,7 @@ SDL_bool SDL_GetWindowWMInfo(SDL_Window * window, struct SDL_SysWMinfo *info)
     }
     return (_this->GetWindowWMInfo(window, info));
 }
-
+// keyboard
 void SDL_StartTextInput(void)
 {
     if (!_this) {
@@ -4656,7 +4656,115 @@ SDL_bool SDL_IsScreenKeyboardShown(SDL_Window *window)
     }
     return SDL_FALSE;
 }
+// clipboard
+int SDL_SetClipboardText(const char *text)
+{
+    if (!_this) {
+        return SDL_SetError("Video subsystem must be initialized to set clipboard text");
+    }
 
+    if (!text) {
+        text = "";
+    }
+    if (_this->SetClipboardText) {
+        return _this->SetClipboardText(text);
+    } else {
+        SDL_free(_this->clipboard_text);
+        _this->clipboard_text = SDL_strdup(text);
+        return 0;
+    }
+}
+
+int SDL_SetPrimarySelectionText(const char *text)
+{
+    if (!_this) {
+        return SDL_SetError("Video subsystem must be initialized to set primary selection text");
+    }
+
+    if (!text) {
+        text = "";
+    }
+    if (_this->SetPrimarySelectionText) {
+        return _this->SetPrimarySelectionText(text);
+    } else {
+        SDL_free(_this->primary_selection_text);
+        _this->primary_selection_text = SDL_strdup(text);
+        return 0;
+    }
+}
+
+char *SDL_GetClipboardText(void)
+{
+    if (!_this) {
+        SDL_SetError("Video subsystem must be initialized to get clipboard text");
+        return SDL_strdup("");
+    }
+
+    if (_this->GetClipboardText) {
+        return _this->GetClipboardText();
+    } else {
+        const char *text = _this->clipboard_text;
+        if (!text) {
+            text = "";
+        }
+        return SDL_strdup(text);
+    }
+}
+
+char *SDL_GetPrimarySelectionText(void)
+{
+    if (!_this) {
+        SDL_SetError("Video subsystem must be initialized to get primary selection text");
+        return SDL_strdup("");
+    }
+
+    if (_this->GetPrimarySelectionText) {
+        return _this->GetPrimarySelectionText();
+    } else {
+        const char *text = _this->primary_selection_text;
+        if (!text) {
+            text = "";
+        }
+        return SDL_strdup(text);
+    }
+}
+
+SDL_bool SDL_HasClipboardText(void)
+{
+    if (!_this) {
+        SDL_SetError("Video subsystem must be initialized to check clipboard text");
+        return SDL_FALSE;
+    }
+
+    if (_this->HasClipboardText) {
+        return _this->HasClipboardText();
+    } else {
+        if (_this->clipboard_text && _this->clipboard_text[0] != '\0') {
+            return SDL_TRUE;
+        } else {
+            return SDL_FALSE;
+        }
+    }
+}
+
+SDL_bool SDL_HasPrimarySelectionText(void)
+{
+    if (!_this) {
+        SDL_SetError("Video subsystem must be initialized to check primary selection text");
+        return SDL_FALSE;
+    }
+
+    if (_this->HasPrimarySelectionText) {
+        return _this->HasPrimarySelectionText();
+    }
+
+    if (_this->primary_selection_text && _this->primary_selection_text[0] != '\0') {
+        return SDL_TRUE;
+    }
+
+    return SDL_FALSE;
+}
+// messagebox
 int SDL_GetMessageBoxCount(void)
 {
     return SDL_AtomicGet(&SDL_messagebox_count);
