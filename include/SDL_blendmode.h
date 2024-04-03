0a6ecf6ae02e0692020406f8a97c37b6074e67eb
pionere <pionere@freemail.hu>
2024-04-03 11:11:59 +0200

    update the documentation of SDL_BlendOperation

--- include/SDL_blendmode.h
+++ include/SDL_blendmode.h
@@ -65,10 +65,10 @@ typedef enum
 typedef enum
 {
     SDL_BLENDOPERATION_ADD              = 0x1,  /**< dst + src: supported by all renderers */
-    SDL_BLENDOPERATION_SUBTRACT         = 0x2,  /**< src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES */
-    SDL_BLENDOPERATION_REV_SUBTRACT     = 0x3,  /**< dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES */
-    SDL_BLENDOPERATION_MINIMUM          = 0x4,  /**< min(dst, src) : supported by D3D9, D3D11 */
-    SDL_BLENDOPERATION_MAXIMUM          = 0x5   /**< max(dst, src) : supported by D3D9, D3D11 */
+    SDL_BLENDOPERATION_SUBTRACT         = 0x2,  /**< src - dst : supported by D3D9, D3D11, D3D12, OpenGL, OpenGLES, Metal, Vulkan */
+    SDL_BLENDOPERATION_REV_SUBTRACT     = 0x3,  /**< dst - src : supported by D3D9, D3D11, D3D12, OpenGL, OpenGLES, Metal, Vulkan */
+    SDL_BLENDOPERATION_MINIMUM          = 0x4,  /**< min(dst, src) : supported by D3D9, D3D11, D3D12, OpenGL, (OpenGLES), Metal, Vulkan */
+    SDL_BLENDOPERATION_MAXIMUM          = 0x5   /**< max(dst, src) : supported by D3D9, D3D11, D3D12, OpenGL, (OpenGLES), Metal, Vulkan */
 } SDL_BlendOperation;
 
 /**
