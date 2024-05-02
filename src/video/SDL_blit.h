d31cd363d624b54454f0d12411746cd530701aa0
pionere <pionere@freemail.hu>
2024-05-02 14:32:25 +0200

    ensure 16-byte alignment for SDL_FillRect*SSE

--- src/video/SDL_blit.h
+++ src/video/SDL_blit.h
@@ -124,6 +124,9 @@ extern SDL_BlitFunc SDL_CalculateBlitA(const SDL_BlitMap *map);
 #elif defined(_MSC_VER)
 #define DECLARE_ALIGNED(t, v, a) __declspec(align(a)) t v
 #else
+#ifdef __SSE__
+#error "unaligned SSE"
+#endif
 #define DECLARE_ALIGNED(t, v, a) t v
 #endif
 
