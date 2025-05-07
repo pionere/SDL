79b75a4af42530e9851e9db3cb39ab22649babbb
pionere <pionere@freemail.hu>
2025-05-07 11:48:08 +0200

    cleanup CPU_haveAltiVec - split the implementations (make it similar to CPU_haveARMSIMD) - return hasVectorUnit directly (__MACOSX__ and __OpenBSD__) - use volatile only in case of setjmp-detection - move the setjmp detection down to CPU_haveAltiVec (fix warnings + deduplicate checks)

--- src/cpuinfo/SDL_cpuinfo.c
+++ src/cpuinfo/SDL_cpuinfo.c
@@ -129,17 +129,6 @@
 #define CPU_CFG2_LSX  (1 << 6)
 #define CPU_CFG2_LASX (1 << 7)
 
-#if defined(SDL_ALTIVEC_INTRINSICS) && defined(HAVE_SETJMP) && !defined(__MACOSX__) && !defined(__OpenBSD__) && !defined(__FreeBSD__) && !(defined(__LINUX__) && defined(HAVE_GETAUXVAL))
-/* This is the brute force way of detecting instruction sets...
-   the idea is borrowed from the libmpeg2 library - thanks!
- */
-static jmp_buf jmpbuf;
-static void illegal_instruction(int sig)
-{
-    longjmp(jmpbuf, 1);
-}
-#endif /* HAVE_SETJMP */
-
 static int CPU_haveCPUID(void)
 {
     int has_CPUID = 0;
@@ -337,30 +326,56 @@ static void CPU_calcCPUIDFeatures(void)
     }
 }
 
+#if defined(SDL_CPUINFO_DISABLED)
 static int CPU_haveAltiVec(void)
 {
-    volatile int altivec = 0;
-#ifndef SDL_CPUINFO_DISABLED
-#if (defined(__MACOSX__) && (defined(__ppc__) || defined(__ppc64__))) || (defined(__OpenBSD__) && defined(__powerpc__))
+    return 0;
+}
+
+#elif (defined(__MACOSX__) && (defined(__ppc__) || defined(__ppc64__))) || (defined(__OpenBSD__) && defined(__powerpc__))
+static int CPU_haveAltiVec(void)
+{
 #ifdef __OpenBSD__
     int selectors[2] = { CTL_MACHDEP, CPU_ALTIVEC };
 #else
     int selectors[2] = { CTL_HW, HW_VECTORUNIT };
 #endif
+    int altivec = 0;
     int hasVectorUnit = 0;
     size_t length = sizeof(hasVectorUnit);
     int error = sysctl(selectors, 2, &hasVectorUnit, &length, NULL, 0);
     if (0 == error) {
-        altivec = (hasVectorUnit != 0);
+        altivec = hasVectorUnit;
     }
+    return altivec;
+}
+
 #elif defined(__FreeBSD__) && defined(__powerpc__)
+static int CPU_haveAltiVec(void)
+{
     unsigned long cpufeatures = 0;
     elf_aux_info(AT_HWCAP, &cpufeatures, sizeof(cpufeatures));
-    altivec = cpufeatures & PPC_FEATURE_HAS_ALTIVEC;
-    return altivec;
+    return cpufeatures & PPC_FEATURE_HAS_ALTIVEC;
+}
+
 #elif defined(__LINUX__) && defined(__powerpc__) && defined(HAVE_GETAUXVAL)
-    altivec = getauxval(AT_HWCAP) & PPC_FEATURE_HAS_ALTIVEC;
+static int CPU_haveAltiVec(void)
+{
+    return getauxval(AT_HWCAP) & PPC_FEATURE_HAS_ALTIVEC;
+}
+
 #elif defined(SDL_ALTIVEC_INTRINSICS) && defined(HAVE_SETJMP)
+/* This is the brute force way of detecting instruction sets...
+   the idea is borrowed from the libmpeg2 library - thanks!
+ */
+static jmp_buf jmpbuf;
+static void illegal_instruction(int sig)
+{
+    longjmp(jmpbuf, 1);
+}
+static int CPU_haveAltiVec(void)
+{
+    volatile int altivec = 0;
     void (*handler)(int sig);
     handler = signal(SIGILL, illegal_instruction);
     if (setjmp(jmpbuf) == 0) {
@@ -369,10 +384,22 @@ static int CPU_haveAltiVec(void)
         altivec = 1;
     }
     signal(SIGILL, handler);
-#endif
-#endif
     return altivec;
 }
+
+#elif defined(__powerpc__)
+static int CPU_haveAltiVec(void)
+{
+#warning CPU_haveAltiVec is not implemented for this PowerPC platform. Write me.
+    return 0;
+}
+
+#else
+static int CPU_haveAltiVec(void)
+{
+    return 0;
+}
+#endif
 
 #if defined(SDL_CPUINFO_DISABLED)
 static int CPU_haveARMSIMD(void)
