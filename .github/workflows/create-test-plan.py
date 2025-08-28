f49003d46a6c2f9a429d55b0521028163f2c8f32
Matthew Zavislak <1291596+elevenfive@users.noreply.github.com>
2025-08-28 16:31:03 -0700

    Add elf alignment check for Android artifacts (#13818)

--- .github/workflows/create-test-plan.py
+++ .github/workflows/create-test-plan.py
@@ -550,6 +550,10 @@ def spec_to_job(spec: JobSpec, key: str, trackmem_symbol_names: bool) -> JobDeta
                     "testmultiaudio-apk",
                     "testsprite-apk",
                 ]
+
+                # -fPIC is required after updating NDK from 21 to 28
+                job.cflags.append("-fPIC")
+                job.cxxflags.append("-fPIC")
         case SdlPlatform.Emscripten:
             job.run_tests = False
             job.shared = False
