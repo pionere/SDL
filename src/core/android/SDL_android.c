13a740e875275edebf3bebfecd9895d3b99d3146
pionere <pionere@freemail.hu>
2025-09-22 10:25:31 +0200

    android: check the whole JNI in nativeSetupJNI

--- src/core/android/SDL_android.c
+++ src/core/android/SDL_android.c
@@ -445,7 +445,6 @@ JNIEXPORT void JNICALL SDL_JAVA_INTERFACE(nativeSetupJNI)(JNIEnv *env, jclass cl
     for (int i = 0; i < SDL_JavaFuncs_count; i++) {
         if (!jnicall[i]) {
             LOGD("Missing Java callback '%s' (idx=%d) of SDLActivity.", SDLActivity_ifc[i].name, i);
-            break;
         }
     }
 
@@ -465,7 +464,6 @@ JNIEXPORT void JNICALL SDL_JAVA_AUDIO_INTERFACE(nativeSetupJNI)(JNIEnv *env, jcl
     for (int i = 0; i < SDL_AudioFuncs_count; i++) {
         if (!jnicall_audio[i]) {
             LOGD("Missing Java callback '%s' (idx=%d) of SDLAudioManager.", SDLAudioManager_ifc[i].name, i);
-            break;
         }
     }
 
@@ -485,7 +483,6 @@ JNIEXPORT void JNICALL SDL_JAVA_CONTROLLER_INTERFACE(nativeSetupJNI)(JNIEnv *env
     for (int i = 0; i < SDL_ControllerFuncs_count; i++) {
         if (!jnicall_ctrl[i]) {
             LOGD("Missing Java callback '%s' (idx=%d) of SDLControllerManager.", SDLControllerManager_ifc[i].name, i);
-            break;
         }
     }
 
